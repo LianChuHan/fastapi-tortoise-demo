5a8b71bdb08a7fb8ed2280cf1757470b0fedd0eb
Lianchuhan <819263042@qq.com>
2024-12-12 19:00:07 +0800

    commit

--- db_models/schemas/circle_of_friends_pydantic.py
+++ db_models/schemas/circle_of_friends_pydantic.py
@@ -4,3 +4,6 @@ from db_models.models.circle_of_friends_model import CircleOfFriends
 
 class CircleOfFriendsPydantic(pydantic_model_creator(CircleOfFriends)):
     pass
+
+class OperatorCircleOfFriendsPydantic(pydantic_model_creator(CircleOfFriends,exclude=("id","create_datetime","update_datetime","is_del"))):
+    pass
